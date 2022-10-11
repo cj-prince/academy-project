27a922a6f5cdeadeb60a6a624146b18acfabaded
cj-prince <davids.chijioke@gmail.com>
2022-10-11 13:33:34 +0100

    intial commit

---
+++ src/components/dashBoard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dashboard">
+    <h1>God's Speed all</h1>
+    <h2>don't for get to create a branch and push to the branch</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landingPage',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DashBoard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DashBoard from '@/components/dashBoard.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    DashBoard
   }
 }
 </script>
