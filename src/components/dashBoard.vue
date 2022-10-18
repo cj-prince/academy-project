b6140adee8118e099bee3e94c5a3ca4931406077
Ade <wunmiokubanjo@gmail.com>
2022-10-18 11:26:35 -0700

    updates

--- src/components/dashBoard.vue
+++ src/components/dashBoard.vue
@@ -7,9 +7,7 @@
         <p> Join enyata academy today and bring your long <br> awaiting dream to reality.</p>
         <input type="submit" value="Register Now"  @click="toRegister">
       </div>
-    
-      </div>
-    </section>
+    </section> 
     <section class="dashBut" >
       <div class="dashButfirst" >
         <p></p>
